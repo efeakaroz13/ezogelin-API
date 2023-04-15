8ba04a2a3de025dacc93128a780b13e5909e50eb
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2023-04-15 18:37:44 +0300

    SSL error solve

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ def getCities():
 @app.route("/getDistricts/<province>")
 def getDistricts_(province):
     listOutput = []
-    page = requests.get("https://cbsapi.tkgm.gov.tr/megsiswebapi.v3/api/IdariYapi/ilceliste/"+province)
+    page = requests.get("https://cbsapi.tkgm.gov.tr/megsiswebapi.v3/api/IdariYapi/ilceliste/"+province,verify=False)
     output = json.loads(page.content)
     output = output["features"]
     for o in output:
@@ -252,7 +252,7 @@ def getDistricts_(province):
 
 @app.route("/nh/<district>")
 def getnh(district):
-    page = requests.get("https://cbsapi.tkgm.gov.tr/megsiswebapi.v3/api/idariYapi/mahalleListe/"+district)
+    page = requests.get("https://cbsapi.tkgm.gov.tr/megsiswebapi.v3/api/idariYapi/mahalleListe/"+district,verify=False)
     listOutput = []
 
     output = json.loads(page.content)
@@ -264,7 +264,7 @@ def getnh(district):
 @app.route("/idari/<id_>/<ada_>/<parsel>")
 def idariSorgu(id_,ada_,parsel):
     try:
-        page = requests.get(f"https://cbsapi.tkgm.gov.tr/megsiswebapi.v3/api/parsel/{id_}/{ada_}/{parsel}")
+        page = requests.get(f"https://cbsapi.tkgm.gov.tr/megsiswebapi.v3/api/parsel/{id_}/{ada_}/{parsel}",verify=False)
     except Exception as e :
         print(e)
         return {"SCC":False,"err":"Couldn't load page"}
